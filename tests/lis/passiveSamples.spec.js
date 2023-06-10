e94b4efc3bb4705c8a0d151060e2bea60da56c37
Ildar1981 <fahriyevi@yandex.ru>
2023-06-10 08:20:29 +0300

    added tests

--- tests/lis/passiveSamples.spec.js
+++ tests/lis/passiveSamples.spec.js
@@ -1,6 +1,6 @@
 import { test } from '../../fixtures/default'
 import { expect } from '@playwright/test'
-import { authAdmin, openModule, searchNapr } from '../../functions'
+import { authAdmin, openModule } from '../../functions'
 import { _patient } from '../../const'
 
 test.describe(`Открытие и закрытие модальных окон`, () => {
@@ -10,7 +10,16 @@ test.describe(`Открытие и закрытие модальных окон`
     await openModule(page, `Лаборатория`)
   })
   test(`окно пассивных проб`, async ({ page }) => {
-    await searchNapr(page, _patient)
+	
+    await page.waitForSelector(`#LisReferralSearch`)
+    await page.locator(`#LisReferralSearch`).fill(_patient);
+    await page.locator(`#LisReferralSearch`).press(`Enter`);
+    expect(page.locator(`text=Направление: Анализ мочи по Нечипоренко`))
+	
+
+    // кликаем по направлению первому из списка
+    await page.locator(`#pane-all div.search-tab > div:nth-child(2) span`).click();
+    await page.waitForTimeout(3000)
     
     // открыть окно пассивных проб
     await page.locator(`#LisFooterBtn_openSamples`).click()
@@ -19,6 +28,27 @@ test.describe(`Открытие и закрытие модальных окон`
     // закрыть окно пассивных проб
     await page.locator(`#LisSamplesDialog div.laba-dialog-title > img`).click()
     expect(page.locator(`#pane-tab1`))
+  })
+  test(`проверка радиобаттонов в пассивных пробах`, async ({ page }) => {
+	
+    await page.waitForSelector(`#LisReferralSearch`)
+    await page.locator(`#LisReferralSearch`).fill(_patient);
+    await page.locator(`#LisReferralSearch`).press(`Enter`);
+    expect(page.locator(`text=Направление: Анализ мочи по Нечипоренко`))
+	
+
+    // кликаем по направлению первому из списка
+    await page.locator(`#pane-all div.search-tab > div:nth-child(2) span`).click();
+    await page.waitForTimeout(3000)
+     // открыть окно пассивных проб
+    await page.locator(`#LisFooterBtn_openSamples`).click()
+    expect(page.locator(`#LisSamplesDialog`))
+
+    // кликаем на радиобаттон все
+    await page.locator(`#LisSamplesSearchUsed > label:nth-child(1) > span.el-radio__input > span`).click()
+    expect(page.locator(`#LisSamplesDialog`))
+
+    
   })
   test(`окно динамики`, async ({ page }) => {
     await page.waitForSelector(`#LisReferralSearch`)
